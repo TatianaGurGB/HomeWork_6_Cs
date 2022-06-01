c559cd656866a17009bb8ae6494f158f1dd01c3b
Tatiana <Tatiana_novik@mail.ru>
2022-06-01 20:41:58 +0700

    HomeWork_6_Cs_all

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void PlusNum ()
+{
+    int result = 0;
+
+    while (true){
+    string num = Console.ReadLine(); 
+    if (num == ""){
+    break;}
+    int a = int.Parse(num);
+    
+    if (a > 0)
+    {
+        result += 1;
+    }   
+    }
+    Console.Write($"Ввели {result} чисел больше 0 ");
+}
+
+PlusNum();
---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+void Dot (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Dot: ({x}; {y})");
+}
+
+Dot(3, 6, 5, 7);
